e33126c84ce0e890a298fadf600c6299b9f9a057
Kevin Zamora <kzamoraquintero@gmail.com>
2025-02-26 08:36:21 -0500

    Desafio Resuelto

---
+++ Solucion_Desafio/solucion_desafio.py
@@ -0,0 +1,30 @@
+# Defino las variables a ingresar
+score_1 = float(input("Ingrese nota 1: "))
+score_2 = float(input("Ingrese nota 2: "))
+score_3 = float(input("Ingrese nota 3: "))
+score_4 = float(input("Ingrese nota 4: "))
+score_5 = float(input("Ingrese nota 5: "))
+
+#Opción 2, solución más versatil
+# n = int( input("Ingrese la cantidad de notas a promediar: "))
+# sum = 0
+# i = 1
+# while (i <= n):
+#     print("Ingrese la nota número: ",i)
+#     score = float(input())
+#     sum= sum + score
+#     i+=1
+
+# Defino los procedimientos aritmeticos a ejecutar
+sum = (score_1 + score_2 + score_3 + score_4 + score_5)
+Avg = sum / 5
+Avg_percent = Avg / 5 * 100
+print ("El promedio de notas es: ", Avg)
+
+# Defino la estructura de control para determinar si el estudiante ha aprobado o no basado en el promedio de sus calificaciones
+if Avg_percent >= 60:
+    print ("Aprobado")
+elif Avg_percent < 40:
+    print ("Reprobado")
+else:
+    print ("En recuperación")
